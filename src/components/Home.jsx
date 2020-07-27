433cac35a2860e55c0a7955aaa914820c503951f
memuna haruna <memuna.haruna@andela.com>
2020-07-27 14:31:01 +0100

    looking good! get it, gurl

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,16 +5,16 @@ import Header from './Header';
 
 const Home = () => {
   const [index, setIndex] = useState(0);
-  const [text] = useState("Hello, I'm  Memuna!");
+  const [text] = useState("Hello, I'm  Memuna. I build cool things that live on the web.");
   const [typed, setTyped] = useState('');
 
-  const [indexTwo, setIndexTwo] = useState(0);
-  const [jobText] = useState("I'm a software engineer xoxo xoxo xoxo");
-  const [typedJobText, setTypedJobText] = useState('');
+  // const [indexTwo, setIndexTwo] = useState(0);
+  // const [jobText] = useState("I'm a software engineer xoxo xoxo xoxo");
+  // const [typedJobText, setTypedJobText] = useState('');
 
   const delay = 50;
   let timer = '';
-  let timerTwo = '';
+  // let timerTwo = '';
 
   useEffect(() => {
     if (index < text.length) {
@@ -27,26 +27,25 @@ const Home = () => {
     return () => clearTimeout(timer);
   }, [index]);
 
-  useEffect(() => {
-    if (indexTwo < jobText.length) {
-      timerTwo = setTimeout(() => {
-        setTypedJobText(typedJobText + jobText[indexTwo]);
-        setIndexTwo(indexTwo + 1);
-      }, delay);
-    }
+  // useEffect(() => {
+  //   if (indexTwo < jobText.length) {
+  //     timerTwo = setTimeout(() => {
+  //       setTypedJobText(typedJobText + jobText[indexTwo]);
+  //       setIndexTwo(indexTwo + 1);
+  //     }, delay);
+  //   }
 
-    return () => clearTimeout(timerTwo);
-  }, [indexTwo]);
+  //   return () => clearTimeout(timerTwo);
+  // }, [indexTwo]);
 
   return (
     <div>
       <Header />
       <section className="home">
-        <div className="home-greeting">
-          <div className="greeting-inner">{typed}</div>
-        </div>
-        <div className="home-job">
-          <div className="job-inner">{typedJobText}</div>
+        <div className="home-description-box">{typed}</div>
+        <div className="home-contact-box">
+          <div className="contact-icons"></div>
+          <div className="home-bottom-box" />
         </div>
       </section>
     </div>
