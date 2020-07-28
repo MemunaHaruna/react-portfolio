230a5c0d223b2b99a3d446b1317a10a88da71858
memuna haruna <memuna.haruna@andela.com>
2020-07-28 03:15:21 +0100

    home page is coming together... I love it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable no-plusplus */
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
@@ -8,13 +11,8 @@ const Home = () => {
   const [text] = useState("Hello, I'm  Memuna. I build cool things that live on the web.");
   const [typed, setTyped] = useState('');
 
-  // const [indexTwo, setIndexTwo] = useState(0);
-  // const [jobText] = useState("I'm a software engineer xoxo xoxo xoxo");
-  // const [typedJobText, setTypedJobText] = useState('');
-
   const delay = 50;
   let timer = '';
-  // let timerTwo = '';
 
   useEffect(() => {
     if (index < text.length) {
@@ -27,24 +25,23 @@ const Home = () => {
     return () => clearTimeout(timer);
   }, [index]);
 
-  // useEffect(() => {
-  //   if (indexTwo < jobText.length) {
-  //     timerTwo = setTimeout(() => {
-  //       setTypedJobText(typedJobText + jobText[indexTwo]);
-  //       setIndexTwo(indexTwo + 1);
-  //     }, delay);
-  //   }
-
-  //   return () => clearTimeout(timerTwo);
-  // }, [indexTwo]);
-
   return (
     <div>
       <Header />
       <section className="home">
         <div className="home-description-box">{typed}</div>
         <div className="home-contact-box">
-          <div className="contact-icons"></div>
+          <div className="contact-icons">
+            <a href="https://github.com/MemunaHaruna">
+              <FontAwesomeIcon icon={faGithub} className="icon" />
+            </a>
+            <a href="https://linkedin.com/in/memuna-ojonugwa-haruna">
+              <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+            </a>
+            <a href="mailto:mmharuna16@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            </a>
+          </div>
           <div className="home-bottom-box" />
         </div>
       </section>
